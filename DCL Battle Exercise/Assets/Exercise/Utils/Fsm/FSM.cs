4344caa1ea0ee9af4a9fd5eac376bd5d4e15d158
Arnaud Briche <arnaudbriche1994@gmail.com>
2024-09-29 14:29:23 +0200

    Changed my mind after trying the HFSM, now using my own version
    I was quite disappointed that HSFM uses string to work, so I decided to use my own FSM instead.

---
+++ DCL Battle Exercise/Assets/Exercise/Utils/Fsm/FSM.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections.Generic;
+
+namespace Utils
+{
+    public abstract class FSM<TState, TStateEnum> : I_LateUpdateOnly, System.IDisposable
+        where TState : class, IFSMState<TStateEnum>
+        where TStateEnum : struct, System.Enum
+    {
+        public TState ActiveState { get; private set; }
+
+        protected readonly Dictionary<TStateEnum, TState> States;
+
+        private readonly List<TStateEnum> _currentStatePotentialTransitions = new(16);
+        private readonly List<TStateEnum> _pendingTransitions = new(4);
+
+        protected FSM(TState defaultState, List<TState> states)
+        {
+            States = new Dictionary<TStateEnum, TState>(states.Count);
+
+            for (int stateIndex = 0; stateIndex < states.Count; stateIndex++)
+            {
+                RegisterNewState(states[stateIndex]);
+            }
+            StartNewState(defaultState, default);
+        }
+
+        public void ManualLateUpdate()
+        {
+            if (!ActiveState.CanBeExited())
+                return;
+
+            // Used as a End of Frame Coroutine to check pending states and chose the one with the highest priority
+            for (int stateIndex = 0; stateIndex < _currentStatePotentialTransitions.Count; stateIndex++)
+            {
+                // we go through each transitions of the state to check for the priority of each state.
+                TStateEnum stateEnum = _currentStatePotentialTransitions[stateIndex];
+                if (_pendingTransitions.Contains(stateEnum))
+                {
+                    StopCurrentState();
+                    StartNewState(States[stateEnum], ActiveState.StateEnum);
+                    _pendingTransitions.Clear();
+                    GameUpdater.Unregister(this);
+                    return;
+                }
+            }
+
+#if DEBUG
+            string transitions = string.Empty;
+            for (int i = 0; i < _pendingTransitions.Count; i++)
+            {
+                transitions += _pendingTransitions[i] + ", ";
+            }
+            throw new System.Exception($"State {ActiveState.StateEnum} couldn't be exited. Pending Transitions: {transitions}");
+#endif
+        }
+
+        public void Dispose()
+        {
+            StopCurrentState();
+            ActiveState = null;
+
+            foreach (TState state in States.Values)
+            {
+                state.OnDestroy();
+            }
+
+            States.Clear();
+            _currentStatePotentialTransitions.Clear();
+            _pendingTransitions.Clear();
+            GameUpdater.Unregister(this);
+        }
+
+        public void RegisterNewState(TState stateToRegister)
+        {
+            if (States.ContainsKey(stateToRegister.StateEnum))
+                throw new System.Exception($"State {stateToRegister.StateEnum} is already registered.");
+
+            States.Add(stateToRegister.StateEnum, stateToRegister);
+        }
+
+        public void RegisterNewState(TStateEnum stateEnum, TState state)
+        {
+            if (States.ContainsKey(stateEnum))
+                throw new System.Exception($"State {stateEnum} is already registered.");
+
+            States.Add(stateEnum, state);
+        }
+
+        public void UnregisterState(TState stateToRemove)
+        {
+            if (!States.Remove(stateToRemove.StateEnum))
+                throw new System.Exception($"State {stateToRemove.StateEnum} isn't registered.");
+        }
+
+        public void UnregisterState(TStateEnum stateToRemove)
+        {
+            if (!States.Remove(stateToRemove))
+                throw new System.Exception($"State {stateToRemove} isn't registered.");
+        }
+
+        public bool TryGetState(TStateEnum stateEnum, out TState state)
+        {
+            return States.TryGetValue(stateEnum, out state);
+        }
+
+        protected virtual void StartNewState(TState newState, TStateEnum oldState)
+        {
+            // StartState and prepare new State
+            ActiveState = newState;
+            ActiveState.IsActiveState = true;
+            newState.StartState(oldState);
+            newState.RequestToExitState += ExitCurrentState;
+
+            _currentStatePotentialTransitions.AddRange(newState.GetTransitionsStates());
+            // some states may be set as potential transitions, but may not be registered in the FSM yet.
+            List<TStateEnum> finalTransitions = new List<TStateEnum>(_currentStatePotentialTransitions.Count);
+
+            for (int stateIndex = 0; stateIndex < _currentStatePotentialTransitions.Count; stateIndex++)
+            {
+                TStateEnum stateEnum = _currentStatePotentialTransitions[stateIndex];
+                if (!States.TryGetValue(stateEnum, out TState state))
+                    continue;
+
+                finalTransitions.Add(stateEnum);
+                state.RequestEnterState += AddPendingState;
+            }
+
+            _currentStatePotentialTransitions.Clear();
+            _currentStatePotentialTransitions.AddRange(finalTransitions);
+            _pendingTransitions.Clear();
+        }
+
+        protected virtual void StopCurrentState()
+        {
+            // EndState and Clean old stateEnum
+            ActiveState.EndState();
+            ActiveState.IsActiveState = false;
+            ActiveState.RequestToExitState -= ExitCurrentState;
+
+            for (int stateIndex = 0; stateIndex < _currentStatePotentialTransitions.Count; stateIndex++)
+            {
+                States[_currentStatePotentialTransitions[stateIndex]].RequestEnterState -= AddPendingState;
+            }
+
+            _currentStatePotentialTransitions.Clear();
+        }
+
+        protected virtual void AddPendingState(TStateEnum newStateEnum)
+        {
+            if (!ActiveState.HasPossibleTransitionsTo(newStateEnum))
+                throw new System.Exception($"State {newStateEnum} isn't part of the possible transitions for {ActiveState.StateEnum}.");
+
+            // The transition was already requested
+            if (!_pendingTransitions.Contains(newStateEnum))
+                _pendingTransitions.Add(newStateEnum);
+
+            GameUpdater.Register(this);
+        }
+
+        private void ExitCurrentState()
+        {
+            for (int stateIndex = 0; stateIndex < _currentStatePotentialTransitions.Count; stateIndex++)
+            {
+                TStateEnum stateEnum = _currentStatePotentialTransitions[stateIndex];
+
+                // The transition was already requested
+                if (_pendingTransitions.Contains(stateEnum))
+                    return;
+
+                TState state = States[stateEnum];
+                if (state.CanBeEntered())
+                {
+                    _pendingTransitions.Add(stateEnum);
+                    GameUpdater.Register(this);
+                    return;
+                }
+            }
+
+            throw new System.Exception($"State {ActiveState} couldn't be exited.");
+        }
+    }
+}
---
+++ DCL Battle Exercise/Assets/Exercise/Utils/Fsm/IFSMState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Utils
+{
+    public interface IFSMState
+    {
+        bool IsActiveState { get; set; }
+        System.Action RequestToExitState { get; set; }
+
+        bool CanBeEntered();
+        bool CanBeExited();
+
+        void OnDestroy();
+    }
+
+    public interface IFSMState<TStateEnum> : IFSMState
+        where TStateEnum : struct, System.Enum
+    {
+        TStateEnum StateEnum { get; }
+
+        System.Action<TStateEnum> RequestEnterState { get; set; }
+
+        bool HasPossibleTransitionsTo(TStateEnum stateEnum);
+
+        void StartState(TStateEnum previousState);
+
+        void EndState();
+
+        List<TStateEnum> GetTransitionsStates();
+    }
+}
