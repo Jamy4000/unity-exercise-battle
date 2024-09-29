87b0733b61c293dcb6e1c78e3398c0583cea7650
Arnaud Briche <arnaudbriche1994@gmail.com>
2024-09-29 21:50:30 +0200

    Introduced a CreateArmy method in BattleInstantiator
    There are so many nested for loops when creating the armies that it was becoming difficult to see what part was responsible for what logic.

--- DCL Battle Exercise/Assets/Exercise/Battle/Scripts/BattleInstantiator.cs
+++ DCL Battle Exercise/Assets/Exercise/Battle/Scripts/BattleInstantiator.cs
@@ -30,60 +30,17 @@ namespace DCLBattle.Battle
         public IArmy GetArmy(int index) => _armies[index];
         public int ArmiesCount => _armies.Length;
 
-
         private static readonly IStrategyUpdater[,] _strategyUpdaters = new IStrategyUpdater[IArmyModel.UnitLength, IStrategyUpdater.StrategyCount];
-
-
 
         void Awake()
         {
             _armies = new IArmy[_armiesToSpawn.Length];
 
-
             // For each army that should spawn on the map
             for (int armyIndex = 0; armyIndex < _armiesToSpawn.Length; armyIndex++)
             {
                 ArmySpawnParameters armySpawnParam = _armiesToSpawn[armyIndex];
-
-                // TODO remove hard implementation
-                IArmy army = new Army(armySpawnParam.ArmyModel);
-                _armies[armyIndex] = army;
-
-                // For each type of unit in the game
-                for (int unitTypeIndex = 0; unitTypeIndex < IArmyModel.UnitLength; unitTypeIndex++)
-                {
-                    // If the current army has this type of unit in its rank
-                    UnitType unitType = (UnitType)unitTypeIndex;
-                    if (armySpawnParam.ArmyModel.TryGetUnitModel(unitType, out IUnitModel model))
-                    {
-                        // No need to recreate the strategy updaters multiple tile, one object for all units of the same type using the same strategy is enough
-                        if (_strategyUpdaters[unitTypeIndex, 0] == null)
-                        {
-                            for (int strategyIndex = 0; strategyIndex < IStrategyUpdater.StrategyCount; strategyIndex++)
-                            {
-                                _strategyUpdaters[unitTypeIndex, strategyIndex] = model.CreateStrategyUpdater((ArmyStrategy)strategyIndex);
-                            }
-                        }
-
-                        // We spawn the amount of units of this type provided in the Launch Menu
-                        int maxUnitCount = armySpawnParam.ArmyModel.GetUnitCount(unitType);
-                        for (int unitIndex = 0; unitIndex < maxUnitCount; unitIndex++)
-                        {
-                            // We generate the parameters to create the Unit
-                            Vector3 position = DCLBattleUtils.GetRandomPosInBounds(armySpawnParam.GetSpawnBounds());
-                            // Could randomize that or make them face a certain direction
-                            Quaternion rotation = Quaternion.identity;
-                            IStrategyUpdater strategyUpdater = _strategyUpdaters[(int)model.UnitType, (int)armySpawnParam.ArmyModel.Strategy];
-
-                            UnitCreationParameters parameters = new(position, rotation, army, model.UnitType, strategyUpdater);
-
-                            // Todo do we really need a model ?
-                            IUnit newUnit = model.InstantiateUnit(parameters);
-                            
-                            army.AddUnit(newUnit);
-                        }
-                    }
-                }
+                _armies[armyIndex] = CreateArmy(armySpawnParam.ArmyModel, armySpawnParam.GetSpawnBounds());
             }
 
             // Registering this as global, but if we want multiple BattleManagers, we could register it on the Scene level as well
@@ -96,6 +53,64 @@ namespace DCLBattle.Battle
             foreach (var army in _armies)
             {
                 army.Update();
+            }
+        }
+
+        private IArmy CreateArmy(IArmyModel armyModel, Bounds spawnBounds)
+        {
+            // TODO remove hard implementation
+            IArmy army = new Army(armyModel);
+
+            // For each type of unit in the game
+            for (int unitTypeIndex = 0; unitTypeIndex < IArmyModel.UnitLength; unitTypeIndex++)
+            {
+                // If the current army has this type of unit in its rank
+                if (armyModel.TryGetUnitModel((UnitType)unitTypeIndex, out IUnitModel unitModel))
+                {
+                    // We check if the strategy updaters were generated for this unit type
+                    CheckForStrategyUpdaterCreation(unitModel);
+
+                    // We create the amount of units provided in the Launch Menu
+                    CreateUnitsForArmy(armyModel, army, unitModel, spawnBounds);
+                }
+            }
+
+            return army;
+        }
+
+        private void CreateUnitsForArmy(IArmyModel armyModel, IArmy army, IUnitModel unitModel, Bounds spawnBounds)
+        {
+            // We spawn the amount of units of this type provided in the Launch Menu
+            int maxUnitCount = armyModel.GetUnitCount(unitModel.UnitType);
+            for (int unitIndex = 0; unitIndex < maxUnitCount; unitIndex++)
+            {
+                // We generate the parameters to create the Unit
+                Vector3 position = DCLBattleUtils.GetRandomPosInBounds(spawnBounds);
+                // Could randomize that or make them face a certain direction
+                Quaternion rotation = Quaternion.identity;
+                // Provides the reference to the strategy Updater that this unit will be using at the start of the battle, based on its army configuration
+                IStrategyUpdater strategyUpdater = _strategyUpdaters[(int)unitModel.UnitType, (int)armyModel.Strategy];
+
+                UnitCreationParameters parameters = new(position, rotation, army, unitModel.UnitType, strategyUpdater);
+
+                IUnit newUnit = unitModel.InstantiateUnit(parameters);
+
+                army.AddUnit(newUnit);
+            }
+        }
+
+        private void CheckForStrategyUpdaterCreation(IUnitModel unitModel)
+        {
+            int unitTypeIndex = (int)unitModel.UnitType;
+
+            // No need to recreate the strategy updaters multiple time, one object for all units of the same type using the same strategy is enough
+            if (_strategyUpdaters[unitTypeIndex, 0] == null)
+            {
+                for (int strategyIndex = 0; strategyIndex < IStrategyUpdater.StrategyCount; strategyIndex++)
+                {
+                    // This will create extra stratyUpdaters that may not be necessary, though we may change design to have a dynamic strategy instead while the battle is ongoing
+                    _strategyUpdaters[unitTypeIndex, strategyIndex] = unitModel.CreateStrategyUpdater((ArmyStrategy)strategyIndex);
+                }
             }
         }
     }
